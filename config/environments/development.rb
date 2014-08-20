d24f0af775f77dea44d18375187c958fb35c6bae
Garrett Goehring <gmgoehring@gmail.com>
2014-08-20 15:02:15 -0500

    add image add w/ paperclip and imagemagick

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+Paperclip.options[:command_path] = "/opt/ImageMagick/bin/convert"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -37,5 +39,7 @@ Rails.application.configure do
 
 #required for heroku
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+
 
 end
